c38084a1d7446446093b5130391a2d3d74181a38
Bruno Viana <brnv1999@gmail.com>
2022-07-07 14:42:58 -0300

    Aprendizado de Lógica Aplicada 1.0

---
+++ códigos/IF_ELSE.js
@@ -0,0 +1,11 @@
+//if else vingadores
+//node ./códigos/IF_ELSE.js
+let vingador = "Pantera Negra"
+
+if( vingador === "Homem de Ferro"){
+    console.log("Sou o melhor vingador")
+} else if(vingador === "Capitão América"){
+    console.log("Cadê o meu escudo?")
+} else {
+    console.log("Hulk esmaga tudo!!!")
+}
